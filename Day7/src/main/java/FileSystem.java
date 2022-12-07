c0659f79a972d1c5023c375306d84fbc32bb913e
Ryan Foulds <Ryan.Foulds@resonate.tech>
2022-12-07 18:20:27 +0000

    Add day 7

---
+++ Day7/src/main/java/FileSystem.java
@@ -0,0 +1,22 @@
+public class FileSystem
+{
+    private Directory currentDirectory;
+
+    public void setCurrentDirectory(final Directory newDirectory)
+    {
+        this.currentDirectory = newDirectory;
+    }
+
+    public Directory getCurrentDirectory()
+    {
+        return currentDirectory;
+    }
+
+    public void cdRoot()
+    {
+        while (currentDirectory.getParent() != null)
+        {
+            currentDirectory = currentDirectory.getParent();
+        }
+    }
+}
