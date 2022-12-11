844ee63365591b0ad4a94c03ae0a330ea92e175e
Ryan Foulds <ryanfoulds@live.com>
2022-12-11 01:16:13 +0000

    refactor reading input on day 10

--- Day10/src/main/java/xyz/foulds/day10/Main.java
+++ Day10/src/main/java/xyz/foulds/day10/Main.java
@@ -1,22 +1,30 @@
 package xyz.foulds.day10;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Main
 {
-    public static void main(final String[] args) throws IOException
+    public static void main(final String[] args)
     {
-        final String input = new String(Files.readAllBytes(Paths.get("src/main/resources/input.txt")));
+        final InputStream inputStream = ClassLoader.getSystemResourceAsStream("input.txt");
+        final List<String> lines = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream),
+                                                                            Charset.defaultCharset()))
+                .lines()
+                .map(String::trim)
+                .collect(Collectors.toList());
+
         final Cpu cpu = new Cpu();
 
         // Part 2 is a side effect of running the cpu instructions.
-        Arrays.stream(input.split("\n"))
-              .map(String::trim)
-              .map(Instruction::fromString)
-              .forEach(cpu::execute);
+        lines.stream()
+             .map(Instruction::fromString)
+             .forEach(cpu::execute);
 
         // Part 2.
         System.out.println(cpu.getSignalStrengthIntegral());
